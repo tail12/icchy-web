d75b6d86472f8fe4f8ee94d7e6dc4ee51c0b165d
icchy <icchy.tech@gmail.com>
2019-12-31 02:34:56 +0900

    first commit

---
+++ content/meta/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: "IcchyLog", // <title>
+  shortSiteTitle: "IcchyLog", // <title> ending for posts and pages
+  siteDescription: "IcchyLog is a site for icchy.",
+  siteUrl: "https://xxxxxxxxxxxxxxxxxxxxxxx.x",
+  // pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "jp",
+
+  /* author */
+  authorName: "いっちー",
+  authorTwitterAccount: "tdall5c",
+
+  /* info */
+  headerTitle: "いっちー",
+  headerSubTitle: "Web Engineer in Japan",
+
+  /* manifest.json */
+  manifestName: "HeroBlog - a GatsbyJS starter",
+  manifestShortName: "IcchyBlog", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  gravatarImgMd5: "",
+
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/ito-lvgs" },
+    { name: "twitter", url: "https://twitter.com/tdall5c" },
+    { name: "facebook", url: "http://facebook.com/" }
+  ]
+};
---
+++ content/posts/2019-12-31--review-this-year/index.md
@@ -0,0 +1,14 @@
+---
+title: "さんぷる的なサムシング"
+category: "dialy"
+cover: photo-1465070845512-2b2dbdc6df66.jpg
+author: いっちー
+---
+
+## H2
+
+```javascript
+import { createMuiTheme } from "material-ui/styles";
+import Color from "color";
+import colors from "./colors";
+```
